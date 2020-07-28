dd0d3ba7f26956d4436f291ee5b6bda2a1e8bece
1160139263 <34066596+1160139263@users.noreply.github.com>
2020-07-28 13:35:02 +0800

    vue文件夹

---
+++ vue-graph/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
